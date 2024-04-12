9d100f3e7af8920fbbe6222535e32ef941cecdcb
Zachary Kano <zkano@wgu.edu>
2024-04-11 19:43:33 -0700

    created new angular class

---
+++ construction-site/src/app/sales-person/sales-person.spec.ts
@@ -0,0 +1,9 @@
+
+
+import { SalesPerson } from './sales-person';
+
+describe('SalesPerson', () => {
+  it('should create an instance', () => {
+    expect(new SalesPerson()).toBeTruthy();
+  });
+});
---
+++ construction-site/src/app/sales-person/sales-person.ts
@@ -0,0 +1,11 @@
+// created by terminal command - ng generate class sales-person/SalesPerson
+
+export class SalesPerson {
+
+  constructor(public firstName: string,
+              public lastName: string,
+              public email: string,
+              public salesVolume: number) {
+
+  }
+}
